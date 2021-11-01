6c84ea07563789d64565d9f6bb189e9ce6143a8e
fabbiocrux <fabbiocrux.ms@gmail.com>
2021-11-01 19:55:58 +0100

    modifying xaringan para que compile desde xaringanthemer

--- docs/xar-WP6/xaringan-themer.R
+++ docs/xar-WP6/xaringan-themer.R
@@ -7,12 +7,35 @@ library(fontawesome)
 
 # set options ------------------------------------------------------------------
 style_duo_accent(
+   # Colors
    primary_color = "#1381B0",
    secondary_color = "#FF961C",
    inverse_header_color = "#FFFFFF", 
+   white_color = "#FFFFFF",
+   
+   # Fonts
    base_font_size = "18px",
    text_font_family = 'Trebuchet-MS',
    header_font_family = 'Trebuchet-MS',
+   
+   # Title slide
+   title_slide_background_color = '#FFFFFF',
+   title_slide_background_image = "https://raw.githubusercontent.com/fabbiocrux/Figures/main/Inedit/INEDIT-logo-00.jpg",
+   title_slide_background_size = '80%',
+   title_slide_background_position = '50% 15%',
+   
+   # Slide content
+   #header_background_auto = 'FALSE',
+   #header_background_color = '#FFFFFF',
+   #header_background_text_color = "#1381B0",
+   #header_background_padding = '0px 0px',
+   #header_background_content_padding_top = "5px",
+   #padding = '25px 50px 50px 25px',
+   
+   # Footnote
+   footnote_position_bottom = '0px',
+   footnote_font_size = "60%",
+
    colors = c(
       red = "#f34213",
       purple = "#3e2f5b",
@@ -26,33 +49,35 @@ style_duo_accent(
    ),
    
    extra_css = list(
+
+      # Changing the Title H1
+      ".remark-slide-content > h1:first-of-type" = list(
+         "margin-top" = "5px",
+         "margin-bottom" = "10px",
+         "border-bottom" = '2px solid #EB811B'
+      ),
+      
       ".remark-slide-content h1" = list(
          "margin-bottom" = 0, 
          "margin-top" = 0),
-      ".remark-slide-content > h1:first-of-type" = list(
-         "margin-top" = "5px",
-         "margin-bottom" = "10px"
-      ),
-      ".title-slide h2" = list("font-family" = "Trebuchet-MS"),
       
+      
+      # Changing the Title H2
       ".remark-slide-content > h2:first-of-type" = list(
          "margin-top" = "5px",
-         "margin-bottom" = "10px"
+         "margin-bottom" = "10px",
+         "border-bottom" = '2px solid #EB811B'
       ),
+      
+      # Changing the Title H3
       ".remark-slide-content > h3:first-of-type" = list(
          "margin-top" = "5px",
          "margin-bottom" = "10px"
       ),
-      ".remark-slide-content" = list (
-         "padding-top" =  "25px",
-         "padding-left" =  "50px",
-         "padding-right" =  "50px",
-         "padding-bottom" =  "25px"
-      ),
+      
+      
       
       ".small" = list("font-size" = "90%"),
-      
-      
       ".Large , .Large .remark-code, .Large .remark-inline-code" = list("font-size"= "144%"), 
       ".large , .large .remark-code, .large .remark-inline-code" = list("font-size"= "120%"), 
       ".small , .small .remark-code, .small .remark-inline-code" = list("font-size"= "90%"), 
@@ -62,24 +87,38 @@ style_duo_accent(
       ".scriptsize , .scriptsize .remark-code, .scriptsize .remark-inline-code" = list("font-size"= "70%"),
       ".tiny , .tiny .remark-code, .tiny .remark-inline-code" = list("font-size"= "60%"),
       
-      ".footnote" = list (
-         "position" =  "absolute",
-         "bottom" =  "0",
-         "padding-right" = "8em", # to avoid the page numbering 
-         "font-size" =  "60%"
-      ),
+
       ".fn" = list(
          "vertical-align" = "super",
          "font-size" = "60%",
          "color" = "purple"
-      ) 
+      ),
+      
+      
+      # Custom classes for Pull Left/Right
+      
+      ".pull-left-2"= list(
+      "float"= "left",
+      "width"= '60%'
+      ),
+      
+      ".pull-right-1"= list(
+         "float"= "right",
+         "width"= '36%'
+      ),
+      
+      ".pull-left-1"= list(
+         "float"= "left",
+         "width"= '36%'
+      ),
+      ".pull-right-2"= list(
+         "float"= "right",
+         "width"= '60%'
+      )
       
       
    ),
    outfile = "assets/xaringan-themer.css"
 )
-
-
-
 
 
