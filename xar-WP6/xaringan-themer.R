b354e5803e461b14b170f105d7d0f0a6e097340f
fabbiocrux <fabbiocrux.ms@gmail.com>
2022-02-09 21:48:34 +0100

    testing names

--- xar-WP6/xaringan-themer.R
+++ xar-WP6/xaringan-themer.R
@@ -20,13 +20,13 @@ style_mono_accent(
    text_font_size = "1rem",
    text_font_family = 'Trebuchet-MS',
    text_font_url = 'https://raw.githubusercontent.com/LF2L/INEDIT-WP6/main/xar-WP6/assets/WP6.css',
-   text_font_weight = "bold",
-   
-   text_font_family_fallback = "Belgates",
+   text_font_weight = 600,
+   #text_font_family_fallback = "serif",
    
    # H1
-   header_font_family = 'Trebuchet-MS',
-   header_font_family_fallback = "mono",
+   header_font_family = 'test-Belgates',
+   #header_font_google  = google_font("Allerta"),
+   #header_font_family_fallback = "Source Code Pro",
    header_h1_font_size = "1.50rem",
    header_font_weight = "bold",
    
